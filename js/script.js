36dc229c11ce9b668ceb84d8cd497e998a6d423e
michelle <16077024@student.hhs.nl>
2021-03-05 22:57:44 +0100

    ander visibility header
    <a toegevoegt om div's te laten zien.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+var divs = ["India", "London", "NewYork", "Sydney", "Warsaw", "Shanghai"];
+    var visibleDivId = null;
+    function divVisibility(divId) {
+      if(visibleDivId === divId) {
+        visibleDivId = null;
+      } else {
+        visibleDivId = divId;
+      }
+      hideNonVisibleDivs();
+    }
+    function hideNonVisibleDivs() {
+      var i, divId, div;
+      for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none";
+        }
+      }
+    }
 /*---------------------------------digitale clock & date-------------------------------*/
 var getIndiaTime = function(){
 document.getElementById("indiaTime").innerHTML = new Date().toLocaleString("en-AU", {timeZone: 'Asia/Kolkata'});
